af761a1184dc46b10878ec8cbc2fd74bbb3fe3b7
care4truth <ananth.peddinti@gmail.com>
2017-10-13 20:14:14 -0600

    1) Adding authentication , authorization

--- src/main/resources/databaseQueries.sql
+++ src/main/resources/databaseQueries.sql
@@ -12,5 +12,9 @@ FOREIGN KEY(username) REFERENCES users(username));
 create unique index ix_auth_username on authorities (username,authority);
 
 
-insert into users(username,password,enabled) values("bryan", "secret" , true);
-insert into authorities(username, authority) values ("bryan","ROLE_USER");
+insert into users(username,password,enabled) values('bryan', 'secret' , true);
+insert into authorities(username, authority) values ('bryan','ROLE_USER');
+
+insert into users(username,password,enabled) values('chris', 'secret' , true);
+insert into authorities(username, authority) values ('chris','ROLE_ADMIN');
+insert into authorities(username, authority) values ('chris','ROLE_USER');
